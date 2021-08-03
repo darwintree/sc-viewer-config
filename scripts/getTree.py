2e98d1270c30885ea5b0f6f2fb1f26391a20c82c
zhangwenda <daerwendeshu@outlook.com>
2021-08-03 15:53:12 +0800

    Add story tree update

---
+++ scripts/getTree.py
@@ -0,0 +1,16 @@
+import os
+import json
+
+def path_to_dict(path):
+    d = {'name': os.path.basename(path)}
+    print(path);
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
+(path)]
+    else:
+        d['type'] = "file"
+    return d
+
+with open("./story_tree.json","w", encoding="utf8") as f:
+    json.dump(path_to_dict('./SCTranslationData/data/story'),f,ensure_ascii=False)
