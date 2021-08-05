3b5e10f5a2d3570ee723304b6076eef0028368fb
zhangwenda <daerwendeshu@outlook.com>
2021-08-05 22:26:49 +0800

    Change idol format

--- scripts/processIdolInfo.py
+++ scripts/processIdolInfo.py
@@ -2,6 +2,8 @@ import json
 
 # rawName 【白いツバサ】櫻木 真乃
 # returns (【白いツバサ】櫻木 真乃, 櫻木 真乃)
+
+
 def processName(rawName):
     # ["【白いツバサ", "櫻木 真乃"]
     tmpSplit = rawName.split("】")
@@ -10,6 +12,7 @@ def processName(rawName):
     cardName = "】".join([tmpSplit[0], idolName])
     return (idolName, cardName)
 
+
 with open('./idol/idol-info.json', 'r', encoding="utf8") as f:
     data = json.load(f)
     idolDict = {}
@@ -17,17 +20,20 @@ with open('./idol/idol-info.json', 'r', encoding="utf8") as f:
         (idolName, cardName) = processName(card['name'])
 
         idolDict.setdefault(idolName, {
-            "ids": [],
+            "list": [],
             "details": {}
         })
         cardId = card['id']
 
         idolDict[idolName]['details'][cardId] = card
         idolDict[idolName]['details'][cardId]['name'] = cardName
-        idolDict[idolName]['ids'].append((cardId, cardName))
+        idolDict[idolName]['list'].append({
+            "id": cardId,
+            "name": cardName
+        })
     for idolName in idolDict:
-        idolDict[idolName]['ids'] = sorted(idolDict[idolName]['ids'], key=lambda x: int(x[0]))
-        with open("./idol/%s.json"%idolName,"w", encoding="utf8") as idolF:
-            json.dump(idolDict[idolName],idolF,ensure_ascii=False,indent=2,separators=(',',':'), sort_keys=True, )
-    
-
+        idolDict[idolName]['list'] = sorted(
+            idolDict[idolName]['list'], key=lambda x: int(x["id"]))
+        with open("./idol/%s.json" % idolName, "w", encoding="utf8") as idolF:
+            json.dump(idolDict[idolName], idolF, ensure_ascii=False,
+                      indent=2, separators=(',', ':'), sort_keys=True, )
