735cf2637071431077e5022e835c1469db89f998
zhangwenda <daerwendeshu@outlook.com>
2021-08-05 10:00:34 +0800

    Process idol info

--- scripts/getTree.py
+++ scripts/getTree.py
@@ -3,7 +3,7 @@ import json
 
 def path_to_dict(path):
     d = {'name': os.path.basename(path)}
-    print(path);
+    # print(path)
     if os.path.isdir(path):
         d['type'] = "directory"
         d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir\
@@ -12,5 +12,5 @@ def path_to_dict(path):
         d['type'] = "file"
     return d
 
-with open("./story-tree.json","w", encoding="utf8") as f:
+with open("./story/story-tree.json","w", encoding="utf8") as f:
     json.dump(path_to_dict('./SCTranslationData/data/story'),f,ensure_ascii=False,indent=2,separators=(',',':'))
